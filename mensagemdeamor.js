dff1331ea398f605ebd22611c8b3e8412ea910f3
Luis Fillipe <luisfillipetava@gmail.com>
2026-01-01 22:40:43 -0300

    Add files via upload

---
+++ mensagemdeamor.js
@@ -0,0 +1,147 @@
+//Variables
+let mobile_media_query = window.matchMedia("(max-width: 400px)");
+let tablet_media_query = window.matchMedia(
+  "(min-width: 400px) and (max-width: 600px)"
+);
+const notes = document.querySelectorAll(".js-note");
+
+//-> Function that resets the size of the notes.
+function recize_notes() {
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].classList.contains("active")) {
+      notes[i].classList.remove("active");
+      gsap.set(notes[i], {
+        height: "30%",
+        clearProps: "all"
+      });
+    }
+  }
+}
+
+//-> Main function that enables all the notes.
+function notes_ready() {
+  gsap.to(".js-envelop-content", {
+    height: "110%",
+    duration: 0.5
+  });
+
+  for (let i = 0; i < notes.length; i++) {
+    notes[i].addEventListener("click", function () {
+      if (mobile_media_query.matches) {
+        if (this.classList.contains("active")) {
+          this.classList.remove("active");
+          gsap.set(this, {
+            height: "30%",
+            clearProps: "all"
+          });
+        } else {
+          for (let i = 0; i < notes.length; i++) {
+            if (notes[i].classList.contains("active")) {
+              notes[i].classList.remove("active");
+              gsap.set(notes[i], {
+                height: "30%",
+                clearProps: "all"
+              });
+            }
+          }
+          this.classList.add("active");
+          gsap.set(this, {
+            height: 125 + 40 * i + "%"
+          });
+        }
+      } else if (tablet_media_query.matches) {
+        if (this.classList.contains("active")) {
+          this.classList.remove("active");
+          gsap.set(this, {
+            height: "30%",
+            clearProps: "all"
+          });
+        } else {
+          for (let i = 0; i < notes.length; i++) {
+            if (notes[i].classList.contains("active")) {
+              notes[i].classList.remove("active");
+              gsap.set(notes[i], {
+                height: "30%",
+                clearProps: "all"
+              });
+            }
+          }
+          this.classList.add("active");
+          gsap.set(this, {
+            height: 80 + 21 * i + "%"
+          });
+        }
+      } else {
+        if (this.classList.contains("active")) {
+          this.classList.remove("active");
+          gsap.set(this, {
+            height: "30%",
+            clearProps: "all"
+          });
+        } else {
+          for (let i = 0; i < notes.length; i++) {
+            if (notes[i].classList.contains("active")) {
+              notes[i].classList.remove("active");
+              gsap.set(notes[i], {
+                height: "30%",
+                clearProps: "all"
+              });
+            }
+          }
+          this.classList.add("active");
+          gsap.set(this, {
+            height: 70 + 20 * i + "%"
+          });
+        }
+      }
+    });
+  }
+}
+
+//-> Function that set up the up paper of the envelope.
+function set_up_paper() {
+  var arr = [0, 0, 100, 0, 50, 61];
+  gsap.set(".js-up-paper", {
+    bottom: "97%",
+    rotation: 180,
+    zIndex: 200,
+    clipPath:
+      "polygon(" +
+      arr[0] +
+      "%" +
+      arr[1] +
+      "%," +
+      arr[2] +
+      "%" +
+      arr[3] +
+      "%," +
+      arr[4] +
+      "%" +
+      arr[5] +
+      "%)",
+    onComplete: notes_ready
+  });
+}
+
+//-> Function that starts the up paper transition.
+function envelop_transition() {
+  gsap.to(".js-up-paper", {
+    bottom: "1%",
+    duration: 0.1, // Mude de 0.25 para 0.1 para ser quase instantâneo
+    onComplete: set_up_paper
+  });
+  document
+    .querySelector(".js-up-paper")
+    .removeEventListener("click", envelop_transition);
+  document.querySelector(".js-up-paper").classList.remove("cursor");
+}
+
+//-> Função que remove o selo e já prepara o envelope para abrir
+function sticker() {
+  gsap.set(".js-sticker", { display: "none" });
+  envelop_transition(); // Isso faz o envelope abrir na hora que clicar no coração!
+}
+
+// Garante que o evento de clique esteja ativo
+document.querySelector(".js-sticker").addEventListener("click", sticker);
+document.querySelector(".js-sticker").addEventListener("touchstart", sticker); // Melhora no celular
